fdefa4a86a754b7f77a2db6b6e3dbb58b3161ff2
Manoj Kumar Reddy Bhimavarapu <cb.en.u4eie17010@cb.students.amrita.edu>
2023-08-29 14:09:48 +0530

    Java src Files Added

---
+++ src/main/java/com/qrcodeexample/qrcodespringboot/QrcodespringbootApplication.java
@@ -0,0 +1,13 @@
+package com.qrcodeexample.qrcodespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodespringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodespringbootApplication.class, args);
+	}
+
+}
